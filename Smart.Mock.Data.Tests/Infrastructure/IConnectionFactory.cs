dc5cf3f5ba1a1672f0cfe1d15c0609ca48a43783
usausa <machi.pon@gmail.com>
2021-11-23 12:20:56 +0900

    Update inspection

--- Smart.Mock.Data.Tests/Infrastructure/IConnectionFactory.cs
+++ Smart.Mock.Data.Tests/Infrastructure/IConnectionFactory.cs
@@ -13,9 +13,7 @@ public static class ConnectionFactoryExtensions
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:ValidateArgumentsOfPublicMethods", Justification = "Ignore")]
     public static T Using<T>(this IConnectionFactory factory, Func<DbConnection, T> func)
     {
-        using (var con = factory.CreateConnection())
-        {
-            return func(con);
-        }
+        using var con = factory.CreateConnection();
+        return func(con);
     }
 }
